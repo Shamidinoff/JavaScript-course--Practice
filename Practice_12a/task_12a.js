4187ff92d4780ad46966abe9bd9fccc7d10d678f
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-07-01 19:06:22 +0600

    Task 12a done

--- Practice_12a/task_12a.js
+++ Practice_12a/task_12a.js
@@ -1,6 +1,6 @@
 // Задача 1: Напишите функцию sumArray, которая принимает массив чисел и возвращает сумму всех чисел в массиве.
 
-const numbers = [1, 2, 3, 4, 5];
+const numbers1 = [1, 2, 3, 4, 5];
 
 function sumArray(arr) {
   let sum = 0;
@@ -10,4 +10,14 @@ function sumArray(arr) {
   return sum;
 }
 
-console.log(sumArray(numbers));
+console.log(sumArray(numbers1));
+
+// Задача 2: Напишите функцию findMax, которая принимает массив чисел и возвращает наибольшее число из массива.
+
+const numbers = [10, 5, 8, 3, 9];
+
+function findMax(array) {
+  return Math.max(...array); // Используем оператор расширения (spread operator) для передачи элементов массива в Math.max
+}
+
+console.log(findMax(numbers));

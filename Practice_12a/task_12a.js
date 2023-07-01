85a93c48289f901f4680f98755a8ddfa59cf7c78
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-07-01 19:14:20 +0600

    Task 12a done

--- Practice_12a/task_12a.js
+++ Practice_12a/task_12a.js
@@ -14,10 +14,22 @@ console.log(sumArray(numbers1));
 
 // Задача 2: Напишите функцию findMax, которая принимает массив чисел и возвращает наибольшее число из массива.
 
-const numbers = [10, 5, 8, 3, 9];
+const numbers2 = [10, 5, 8, 3, 9];
 
 function findMax(array) {
   return Math.max(...array); // Используем оператор расширения (spread operator) для передачи элементов массива в Math.max
 }
 
-console.log(findMax(numbers));
+console.log(findMax(numbers2));
+
+// Задача 3: Напишите функцию filterEvenNumbers, которая принимает массив чисел и возвращает новый массив, содержащий только четные числа из исходного массива.
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function filterEvenNumbers(arrayNum) {
+  return arrayNum.filter((element) => {
+    return element % 2 === 0; // Возвращаем true для четных чисел, которые должны быть включены в новый массив
+  });
+}
+
+console.log(filterEvenNumbers(numbers)); // Ожидаемый вывод: [2, 4, 6, 8, 10]

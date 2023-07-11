585eb1feb3e5e87f575ff02be3acb3507823be3b
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-07-11 22:58:24 +0600

    Task 16b Food dist timer

--- Practice_16b_food_dist_timer/js/script.js
+++ Practice_16b_food_dist_timer/js/script.js
@@ -39,7 +39,7 @@ window.addEventListener("DOMContentLoaded", function () {
 
   // Timer
 
-  const deadline = "2020-05-11";
+  const deadline = "2023-07-30";
 
   function getTimeRemaining(endtime) {
     const t = Date.parse(endtime) - Date.parse(new Date());
@@ -47,5 +47,37 @@ window.addEventListener("DOMContentLoaded", function () {
     const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
     const minutes = Math.floor((t / 1000 / 60) % 60);
     const seconds = Math.floor((t / 1000) % 60);
+
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
   }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector);
+    const days = timer.querySelector("#days");
+    const hours = timer.querySelector("#hours");
+    const minutes = timer.querySelector("#minutes");
+    const seconds = timer.querySelector("#seconds");
+    const timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      days.innerHTML = t.days;
+      hours.innerHTML = t.hours;
+      minutes.innerHTML = t.minutes;
+      seconds.innerHTML = t.seconds;
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock(".timer", deadline);
 });

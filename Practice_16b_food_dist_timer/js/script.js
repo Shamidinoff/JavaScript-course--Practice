486f82dad31fdf9ec99ab13a2db4d70749804776
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-07-17 22:42:59 +0600

    Task 16b Food dist timer - add modal window

--- Practice_16b_food_dist_timer/js/script.js
+++ Practice_16b_food_dist_timer/js/script.js
@@ -147,12 +147,13 @@ window.addEventListener("DOMContentLoaded", function () {
   // Class for card
 
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
+      this.classes = classes;
       this.parent = document.querySelector(parentSelector);
       this.transfer = 27;
       this.changeToUAH();
@@ -164,8 +165,9 @@ window.addEventListener("DOMContentLoaded", function () {
 
     render() {
       const element = document.createElement("div");
+      this.classes.forEach((className) => element.classList.add(className));
       element.innerHTML = `
-      <div class="menu__item">
+
       <img src=${this.src} alt=${this.alt} />
       <h3 class="menu__item-subtitle">${this.title}</h3>
       <div class="menu__item-descr">
@@ -176,7 +178,7 @@ window.addEventListener("DOMContentLoaded", function () {
         <div class="menu__item-cost">Цена:</div>
         <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
       </div>
-    </div>
+
       `;
       this.parent.append(element);
     }
@@ -188,7 +190,8 @@ window.addEventListener("DOMContentLoaded", function () {
     'Меню "Фитнес"',
     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
     9,
-    ".menu .container"
+    ".menu .container",
+    "menu__item"
   ).render();
 
   new MenuCard(
@@ -197,7 +200,8 @@ window.addEventListener("DOMContentLoaded", function () {
     'Меню "Постное"',
     "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
     14,
-    ".menu .container"
+    ".menu .container",
+    "menu__item"
   ).render();
 
   new MenuCard(
@@ -206,6 +210,7 @@ window.addEventListener("DOMContentLoaded", function () {
     "Меню “Премиум”",
     "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
     21,
-    ".menu .container"
+    ".menu .container",
+    "menu__item"
   ).render();
 });

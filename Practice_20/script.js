9c1507d1e42188a0168fa563b3ba40734900a26f
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-07-22 13:38:56 +0600

    Task 20 done

--- Practice_20/script.js
+++ Practice_20/script.js
@@ -12,6 +12,8 @@
 // 3) Напишите функцию setFilmsIds, которая будет принимать этот же массив, а возвращать будет такой же массив с фильмами, но у каждого фильма будет новое поле id. Значение этого поля установите по нумерации фильма.
 // Пример:
 // setFilmsIds(films)  => [   { name: 'Titanic', rating: 9, id: 0 },   { name: 'Die hard 5', rating: 5, id: 1 },   { name: 'Matrix', rating: 8, id: 2 },   { name: 'Some bad film', rating: 4, id: 3 } ]
+// 4) Запишите результат предыдущей функции в переменную tranformedArray. Напишите функцию checkFilms, которая будет проверять, что в каждом из фильмов есть поле id. Если это так - функция возвращает true. Очевидно, что сейчас условие должно выполняться, если мы передаем checkFilms(tranformedArray); :)
+// P.S. Вот тут вы столкнетесь с интересным моментом, который я хочу, чтобы вы запомнили. Внимательно проследите за тем, что происходит внутри коллбэка и что будет проверяться. Дополнительно расписал этот момент в комментариях в ответах.
 
 const films = [
   {
@@ -48,15 +50,19 @@ const films = [
 
 // showListOfFilms(films);
 
-function setFilmsIds(arr) {
-  return arr.map((film, i) => {
-    film.id = i;
-    return film;
-  });
-}
+// function setFilmsIds(arr) {
+//   return arr.map((film, i) => {
+//     film.id = i;
+//     return film;
+//   });
+// }
 
-setFilmsIds(films);
+// setFilmsIds(films);
 
 const tranformedArray = setFilmsIds(films);
 
-function checkFilms(arr) {}
+function checkFilms(arr) {
+  return arr.every((film) => (film.id || film.id === 0 ? true : false));
+}
+
+checkFilms(tranformedArray);

b591d63e8ae323186eae7aaa7fc43b2f6e7153f6
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-07-22 13:32:40 +0600

    Task 20 done

--- Practice_20/script.js
+++ Practice_20/script.js
@@ -6,6 +6,12 @@
 // Задачи:
 // 1) У вас есть список фильмов с рейтингом в виде массива объектов. Напишите функцию showGoodFilms, которая будет принимать этот массив, а возвращать будет массив объектов только с теми фильмами, у которых рейтинг больше или равен 8.
 // P.S. Это довольно типичная задача в программировании. Вспомните, на самых разных сайтах можно так фильтровать любые товары/фильмы/сериалы...
+// 2) Напишите функцию showListOfFilms, которая будет принимать этот же массив, а возвращать будет строку, которая содержит названия фильмов через запятую.
+// Пример:
+// showListOfFilms(films) => "Titanic, Die hard 5, Matrix, Some bad film"
+// 3) Напишите функцию setFilmsIds, которая будет принимать этот же массив, а возвращать будет такой же массив с фильмами, но у каждого фильма будет новое поле id. Значение этого поля установите по нумерации фильма.
+// Пример:
+// setFilmsIds(films)  => [   { name: 'Titanic', rating: 9, id: 0 },   { name: 'Die hard 5', rating: 5, id: 1 },   { name: 'Matrix', rating: 8, id: 2 },   { name: 'Some bad film', rating: 4, id: 3 } ]
 
 const films = [
   {
@@ -26,17 +32,31 @@ const films = [
   },
 ];
 
-function showGoodFilms(arr) {
-  return arr.filter(function (item) {
-    return item.rating >= 8;
+// function showGoodFilms(arr) {
+//   return arr.filter(function (item) {
+//     return item.rating >= 8;
+//   });
+// }
+
+// showGoodFilms(films);
+
+// function showListOfFilms(arr) {
+//   return arr.reduce(
+//     (acc, curr) => `${typeof acc === "object" ? acc.name : acc}, ${curr.name}`
+//   );
+// }
+
+// showListOfFilms(films);
+
+function setFilmsIds(arr) {
+  return arr.map((film, i) => {
+    film.id = i;
+    return film;
   });
 }
 
-showGoodFilms(films);
-
-function showListOfFilms(arr) {}
-
-function setFilmsIds(arr) {}
+const filmsIds = setFilmsIds(films);
+console.log(filmsIds);
 
 const tranformedArray = setFilmsIds(films);
 

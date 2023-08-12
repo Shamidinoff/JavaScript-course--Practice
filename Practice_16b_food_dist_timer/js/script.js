ebca01c6821f077a4e6642981c9ed32e62bca216
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-08-12 23:16:56 +0600

    Task 16b Food dist timer - add json-server file and npm install

--- Practice_16b_food_dist_timer/js/script.js
+++ Practice_16b_food_dist_timer/js/script.js
@@ -182,20 +182,31 @@ window.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  // getResource("http://localhost:3000/menu").then((data) => {
-  //   data.forEach(({ img, altimg, title, descr, price }) => {
-  //     new MenuCard(
-  //       img,
-  //       altimg,
-  //       title,
-  //       descr,
-  //       price,
-  //       ".menu .container"
-  //     ).render();
-  //   });
-  // });
+  getResource("http://localhost:3000/menu").then((data) => {
+    data.forEach(({ img, altimg, title, descr, price }) => {
+      new MenuCard(
+        img,
+        altimg,
+        title,
+        descr,
+        price,
+        ".menu .container"
+      ).render();
+    });
+  });
 
-  axios.get("http://localhost:3000/menu").then((data) => console.log(data));
+  // AXIOS example
+
+  // axios.get("http://localhost:3000/menu").then((data) => data.data.forEach(({ img, altimg, title, descr, price }) => {
+  //   new MenuCard(
+  //     img,
+  //     altimg,
+  //     title,
+  //     descr,
+  //     price,
+  //     ".menu .container"
+  //   ).render();
+  // }););
 
   // Forms
 

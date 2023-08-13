3452cbead8395f9d748a7fb4a37dee9ab06b747d
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-08-13 20:48:33 +0600

    Task 16b Food dist - add slider

--- Practice_16b_food_dist_timer/js/script.js
+++ Practice_16b_food_dist_timer/js/script.js
@@ -306,43 +306,43 @@ window.addEventListener("DOMContentLoaded", function () {
   const total = document.querySelector("#total");
   const current = document.querySelector("#current");
 
-  showSlides(slideIndex);
+  // showSlides(slideIndex);
 
-  if (slides.length < 10) {
-    total.textContent = `0${slides.length}`;
-  } else {
-    total.textContent = slides.length;
-  }
+  // if (slides.length < 10) {
+  //   total.textContent = `0${slides.length}`;
+  // } else {
+  //   total.textContent = slides.length;
+  // }
 
-  function showSlides(n) {
-    if (n > slides.length) {
-      slideIndex = 1;
-    }
+  // function showSlides(n) {
+  //   if (n > slides.length) {
+  //     slideIndex = 1;
+  //   }
 
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
+  //   if (n < 1) {
+  //     slideIndex = slides.length;
+  //   }
 
-    slides.forEach((item) => (item.style.display = "none"));
+  //   slides.forEach((item) => (item.style.display = "none"));
 
-    slides[slideIndex - 1].style.display = "block";
+  //   slides[slideIndex - 1].style.display = "block";
 
-    if (slides.length < 10) {
-      current.textContent = `0${slideIndex}`;
-    } else {
-      current.textContent = slideIndex;
-    }
-  }
+  //   if (slides.length < 10) {
+  //     current.textContent = `0${slideIndex}`;
+  //   } else {
+  //     current.textContent = slideIndex;
+  //   }
+  // }
 
-  function plusSlides(n) {
-    showSlides((slideIndex += n));
-  }
+  // function plusSlides(n) {
+  //   showSlides((slideIndex += n));
+  // }
 
-  prev.addEventListener("click", () => {
-    plusSlides(-1);
-  });
+  // prev.addEventListener("click", () => {
+  //   plusSlides(-1);
+  // });
 
-  next.addEventListener("click", () => {
-    plusSlides(+1);
-  });
+  // next.addEventListener("click", () => {
+  //   plusSlides(+1);
+  // });
 });

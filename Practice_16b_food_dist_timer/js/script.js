a8fe57e33d16fa5744f6bf8d712764a3fb0581ab
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-08-13 21:04:01 +0600

    Task 16b Food dist - add advanced slider

--- Practice_16b_food_dist_timer/js/script.js
+++ Practice_16b_food_dist_timer/js/script.js
@@ -299,12 +299,36 @@ window.addEventListener("DOMContentLoaded", function () {
 
   // slider
 
-  let slideIndex = 1;
   const slides = document.querySelectorAll(".offer__slide");
   const prev = document.querySelector(".offer__slider-prev");
   const next = document.querySelector(".offer__slider-next");
   const total = document.querySelector("#total");
   const current = document.querySelector("#current");
+  const slidesWrapper = document.querySelector(".offer__slider-wrapper");
+  const slidesField = document.querySelector(".offer__slider-inner");
+  const width = window.getComputedStyle(slidesWrapper).width;
+  let slideIndex = 1;
+  let offset = 0;
+
+  slidesField.style.width = 100 * slides.length + "%";
+  slidesField.style.display = "flex";
+  slidesField.style.transition = "0.5s all";
+
+  slidesWrapper.style.overflow = "hidden";
+
+  slides.forEach((slide) => {
+    slide.style.width = width;
+  });
+
+  next.addEventListener("click", () => {
+    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+      offset = 0;
+    } else {
+      offset += +width.slice(0, width.length - 2);
+    }
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
+  });
 
   // showSlides(slideIndex);
 

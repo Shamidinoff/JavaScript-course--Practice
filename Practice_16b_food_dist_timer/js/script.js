7a191a7268cbac3dedf846c4682d4ccaf54117d8
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-08-13 20:14:10 +0600

    Task 16b Food dist - add slider

--- Practice_16b_food_dist_timer/js/script.js
+++ Practice_16b_food_dist_timer/js/script.js
@@ -299,5 +299,37 @@ window.addEventListener("DOMContentLoaded", function () {
 
   // slider
 
-  const slides = document.querySelector("");
+  const slides = document.querySelector(".offer__slide");
+  const prev = document.querySelector(".offer__slider-prev");
+  const next = document.querySelector(".offer__slider-next");
+
+  let slideIndex = 1;
+
+  showSlides(slideIndex);
+
+  function showSlides(n) {
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+
+    slides.forEach((item) => (item.style.display = "none"));
+
+    slides[slideIndex - 1].style.display = "block";
+  }
+
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+
+  prev.addEventListener("click", () => {
+    plusSlides(-1);
+  });
+
+  next.addEventListener("click", () => {
+    plusSlides(+1);
+  });
 });

042252a078e701fb05e705c963d407f7df5e4854
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-08-14 23:09:56 +0600

    Task 16b Food dist - add calculator for health

--- Practice_16b_food_dist_timer/js/script.js
+++ Practice_16b_food_dist_timer/js/script.js
@@ -474,8 +474,29 @@ window.addEventListener("DOMContentLoaded", function () {
 
   calcTotal();
 
-  function getStaticInformation(parentSelector, activeClass) {
-    const elements = document.querySelectorAll(`${parentSelector} div`);
+  function initLocalSettings(selector, activeClass) {
+    const elements = document.querySelectorAll(selector);
+
+    elements.forEach((elem) => {
+      elem.classList.remove(activeClass);
+      if (elem.getAttribute("id") === localStorage.getItem("sex")) {
+        elem.classList.add(activeClass);
+      }
+
+      if (elem.getAttribute("data-ratio") === localStorage.getItem("ratio")) {
+        elem.classList.add(activeClass);
+      }
+    });
+  }
+
+  initLocalSettings("#gender div", "calculating__choose-item_active");
+  initLocalSettings(
+    ".calculating__choose_big div",
+    "calculating__choose-item_active"
+  );
+
+  function getStaticInformation(selector, activeClass) {
+    const elements = document.querySelectorAll(selector);
 
     elements.forEach((elem) => {
       elem.addEventListener("click", (e) => {
@@ -498,9 +519,9 @@ window.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  getStaticInformation("#gender", "calculating__choose-item_active");
+  getStaticInformation("#gender div", "calculating__choose-item_active");
   getStaticInformation(
-    ".calculating__choose_big",
+    ".calculating__choose_big div",
     "calculating__choose-item_active"
   );
 

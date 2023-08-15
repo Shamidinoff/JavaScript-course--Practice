d5c4023d47298e09a03395ab244cc9cec3c874c8
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-08-15 20:46:48 +0600

    Task 16b Food dist - add webpack

--- Practice_16b_food_dist_timer/js/script.js
+++ Practice_16b_food_dist_timer/js/script.js
@@ -1,42 +1,4 @@
 window.addEventListener("DOMContentLoaded", function () {
-  // Tabs
-
-  let tabs = document.querySelectorAll(".tabheader__item"),
-    tabsContent = document.querySelectorAll(".tabcontent"),
-    tabsParent = document.querySelector(".tabheader__items");
-
-  function hideTabContent() {
-    tabsContent.forEach((item) => {
-      item.classList.add("hide");
-      item.classList.remove("show", "fade");
-    });
-
-    tabs.forEach((item) => {
-      item.classList.remove("tabheader__item_active");
-    });
-  }
-
-  function showTabContent(i = 0) {
-    tabsContent[i].classList.add("show", "fade");
-    tabsContent[i].classList.remove("hide");
-    tabs[i].classList.add("tabheader__item_active");
-  }
-
-  hideTabContent();
-  showTabContent();
-
-  tabsParent.addEventListener("click", function (event) {
-    const target = event.target;
-    if (target && target.classList.contains("tabheader__item")) {
-      tabs.forEach((item, i) => {
-        if (target == item) {
-          hideTabContent();
-          showTabContent(i);
-        }
-      });
-    }
-  });
-
   // Timer
 
   const deadline = "2023-09-30";
@@ -138,62 +100,6 @@ window.addEventListener("DOMContentLoaded", function () {
     }
   }
   window.addEventListener("scroll", showModalByScroll);
-
-  // Используем классы для создание карточек меню
-
-  class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
-      this.src = src;
-      this.alt = alt;
-      this.title = title;
-      this.descr = descr;
-      this.price = price;
-      this.classes = classes;
-      this.parent = document.querySelector(parentSelector);
-      this.transfer = 27;
-      this.changeToUAH();
-    }
-
-    changeToUAH() {
-      this.price = this.price * this.transfer;
-    }
-
-    render() {
-      const element = document.createElement("div");
-
-      if (this.classes.length === 0) {
-        this.classes = "menu__item";
-        element.classList.add(this.classes);
-      } else {
-        this.classes.forEach((className) => element.classList.add(className));
-      }
-
-      element.innerHTML = `
-              <img src=${this.src} alt=${this.alt}>
-              <h3 class="menu__item-subtitle">${this.title}</h3>
-              <div class="menu__item-descr">${this.descr}</div>
-              <div class="menu__item-divider"></div>
-              <div class="menu__item-price">
-                  <div class="menu__item-cost">Цена:</div>
-                  <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-              </div>
-          `;
-      this.parent.append(element);
-    }
-  }
-
-  getResource("http://localhost:3000/menu").then((data) => {
-    data.forEach(({ img, altimg, title, descr, price }) => {
-      new MenuCard(
-        img,
-        altimg,
-        title,
-        descr,
-        price,
-        ".menu .container"
-      ).render();
-    });
-  });
 
   // Forms
 

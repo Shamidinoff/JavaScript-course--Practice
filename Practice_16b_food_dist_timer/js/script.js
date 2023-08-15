ac1724d3e3e6fa400dbca5167b0e7db0b1542ba1
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-08-15 21:02:57 +0600

    Task 16b Food dist - add webpack

--- Practice_16b_food_dist_timer/js/script.js
+++ Practice_16b_food_dist_timer/js/script.js
@@ -1,1 +1,17 @@
-window.addEventListener("DOMContentLoaded", function () {});
+window.addEventListener("DOMContentLoaded", function () {
+  const tabs = require("./modules/tabs"),
+    modal = require("./modules/modal"),
+    calc = require("./modules/calc"),
+    cards = require("./modules/cards"),
+    forms = require("./modules/forms"),
+    slider = require("./modules/slider"),
+    timer = require("./modules/timer");
+
+  tabs();
+  modal();
+  calc();
+  cards();
+  forms();
+  slider();
+  timer();
+});
